ab3fd584031ecae29851546e7b2ff35560aa9e7a
annatreat <at3862@barnard.edu>
2024-07-25 11:57:47 -0400

    changes

--- Anna/serotonin_mobility_analysis.py
+++ Anna/serotonin_mobility_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scipy.stats import ttest_ind
 import numpy as np
+from datetime import datetime
 
 def load_and_merge_data(control_mobility_paths, control_serotonin_paths, pcb_mobility_paths, pcb_serotonin_paths):
     def load_data(paths):
@@ -132,3 +133,68 @@ def calculate_total_distance(file_path, likelihood_threshold=0.8):
     total_distance = dist_moved.sum()
 
     return total_distance
+
+
+def read_and_process_signal(block_path, serotonin_channel='_465A', isos_channel='_405A'):
+    block = read_block(block_path)
+    x1 = block['streams'][serotonin_channel].data
+    x2 = block['streams'][isos_channel].data
+
+    reg = np.polyfit(x2, x1, 1)
+
+    if reg[0] < 0:
+        f0 = np.mean(x1)
+    else:
+        f0 = reg[0] * x2 + reg[1]
+
+    delF = 100 * (x1 - f0) / f0
+    return delF, block
+
+def calculate_zscore_signal(corrected_signal, baseline_end_idx):
+    baseline_mean = np.mean(corrected_signal[:baseline_end_idx])
+    baseline_sd = np.std(corrected_signal[:baseline_end_idx])
+    zscore_signal = (corrected_signal - baseline_mean) / baseline_sd
+    return zscore_signal
+
+def process_behavior_data(h5_path):
+    beh_df = pd.read_hdf(h5_path)
+
+    x_coords = beh_df[(beh_df.columns.values[0][0], 'back1', 'x')]
+    y_coords = beh_df[(beh_df.columns.values[0][0], 'back1', 'y')]
+
+    valid_points = (x_coords > 0) & (y_coords > 0)
+    likelihood_filter = beh_df[('DLC_resnet50_pcb_testJul2shuffle1_1030000', 'back1', 'likelihood')] > 0.8
+    x_coords = x_coords[valid_points & likelihood_filter]
+    y_coords = y_coords[valid_points & likelihood_filter]
+
+    dist_moved = np.sqrt(np.diff(x_coords)**2 + np.diff(y_coords)**2)
+    velocity = dist_moved / (1 / 30)
+
+    mean_velocity = velocity.mean()
+    total_distance = dist_moved.sum()
+
+    return mean_velocity, total_distance
+
+def load_and_merge_data(velocity_path, serotonin_path):
+    velocity_data = pd.read_csv(velocity_path)
+    serotonin_data = pd.read_csv(serotonin_path)
+    serotonin_data = serotonin_data.iloc[::100, :]
+    
+    velocity_data.rename(columns={'Adjusted Time (seconds)': 'Time (seconds)'}, inplace=True)
+    serotonin_data.rename(columns={'Time (s)': 'Time (seconds)'}, inplace=True)
+    
+    velocity_data = velocity_data[velocity_data['Smoothed Velocity (cm/s)'] <= 450]
+    merged_data = pd.merge_asof(velocity_data, serotonin_data, on='Time (seconds)', direction='nearest')
+    merged_data.dropna(inplace=True)
+
+    return merged_data
+
+def preprocess_signal_data(saline_path, pcb_path, serotonin_channel='_465A', isos_channel='_405A'):
+    delF_saline, saline_block = read_and_process_signal(saline_path, serotonin_channel, isos_channel)
+    delF_pcb, pcb_block = read_and_process_signal(pcb_path, serotonin_channel, isos_channel)
+
+    return delF_saline, saline_block, delF_pcb, pcb_block
+
+def calculate_corrected_signal(delF, block, isos_channel='_405A'):
+    corrected_signal = delF - block['streams'][isos_channel].data
+    return corrected_signal
