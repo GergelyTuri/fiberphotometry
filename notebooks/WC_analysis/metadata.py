9f97539e00a7b4ec1d7f66483b2f0bb377b2efea
shaharia-khan <shaharia.kn@gmail.com>
2025-01-24 18:47:42 -0500

    Modified PSTH to show error bars

--- notebooks/WC_analysis/metadata.py
+++ notebooks/WC_analysis/metadata.py
@@ -1,23 +1,47 @@
 # Housekeeping for metadata
 
-from os.path import join
+from os.path import join, exists 
 
 # Path to the root data directory
-ROOT_DATA_DIR = "/mnt/DataDrive1/shaharia/Test_Data"
+# ROOT_DATA_DIR = "/mnt/DataDrive1/shaharia/Test_Data"
+
+
+# clean up metadata
+#CLEANUP_METADATA = join(ROOT_DATA_DIR, "data_cleanup.xlsx")
+
+# cleaned up metadata
+#try:
+    #CLEANED_METADATA = join(ROOT_DATA_DIR, "data_cleanup_parsed.xlsx")
+#except FileNotFoundError:
+    #CLEANED_METADATA = None
+    #print("CLEANED_METADATA not found")
+
+# good quality metadata
+#try:
+    #GOOD_METADATA = join(ROOT_DATA_DIR, "data_cleanup_parsed_good.xlsx")
+#except FileNotFoundError:
+    #GOOD_METADATA = None
+    #print("GOOD_METADATA not found")
+
+
+##############################################################################
+
+# Modified script
+
+# Path to the root data directory
+ROOT_DATA_DIR = "C:/Users/khansha/Test_Data"
 
 # clean up metadata
 CLEANUP_METADATA = join(ROOT_DATA_DIR, "data_cleanup.xlsx")
 
 # cleaned up metadata
-try:
-    CLEANED_METADATA = join(ROOT_DATA_DIR, "data_cleanup_parsed.xlsx")
-except FileNotFoundError:
+CLEANED_METADATA = join(ROOT_DATA_DIR, "data_cleanup_parsed.xlsx")
+if not exists(CLEANED_METADATA):
     CLEANED_METADATA = None
     print("CLEANED_METADATA not found")
 
 # good quality metadata
-try:
-    GOOD_METADATA = join(ROOT_DATA_DIR, "data_cleanup_parsed_good.xlsx")
-except FileNotFoundError:
+GOOD_METADATA = join(ROOT_DATA_DIR, "data_cleanup_parsed_good.xlsx")
+if not exists(GOOD_METADATA):
     GOOD_METADATA = None
     print("GOOD_METADATA not found")
